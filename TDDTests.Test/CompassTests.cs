ef6a46e6d5b21564506ab0db92111ca84c99ae3f
Yosefgid <jossykasshun@yahoo.com>
2026-03-23 13:33:16 +0000

    Feat: Add test project

---
+++ TDDTests.Test/CompassTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDTests.Test
+{
+    public class CompassTests
+    {
+        [Test]
+        //Return x when passed y
+        //Ensure that when the rotate method is passed a point
+        //Facing North and a Right direction it should return Point.East
+        public void Rotate_East_When_Rotating_North_Right()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
