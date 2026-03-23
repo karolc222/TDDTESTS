71d3902f82017a5b9dd2621f9fef0d3443aaf1a2
Yosefgid <jossykasshun@yahoo.com>
2026-03-23 11:52:11 +0000

    Add project files.

---
+++ TDDTests.Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TDDTests.Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
